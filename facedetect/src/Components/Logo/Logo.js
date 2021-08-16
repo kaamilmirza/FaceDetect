a08c486f45908b234ab6ec5fe07b90a4b6eda373
kaamilmirza <kaamil.mirza.2002@gmail.com>
2021-08-16 23:57:28 +0530

    still lazy

--- facedetect/src/Components/Logo/Logo.js
+++ facedetect/src/Components/Logo/Logo.js
@@ -1,8 +1,15 @@
 import React from 'react';
-
+import Tilt from 'react-tilt';
+import './logo.css';
 const Logo = () =>{
     return(
-        <div className= "ma4 mt0"></div>
+        <div className= "ma4 mt0">
+            <Tilt className="Tilt br2 shadow-2" options={{ max : 25 }} style={{ height: 250, width: 250 }} >
+                <div className="Tilt-inner"> 👽 </div>
+            </Tilt>
+            
+        </div>
+
     );
 }
 
