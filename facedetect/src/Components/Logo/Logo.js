e2a59eae9b86e3f291edef99d427156369000059
kaamilmirza <kaamil.mirza.2002@gmail.com>
2021-08-17 00:48:27 +0530

    done for night

--- facedetect/src/Components/Logo/Logo.js
+++ facedetect/src/Components/Logo/Logo.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import Tilt from 'react-tilt';
 import './logo.css';
+import brain from './icons8-brain-50.png';
 const Logo = () =>{
     return(
-        <div className= "ma4 mt0">
+        <div className= "ma4 mt0" >
             <Tilt className="Tilt br2 shadow-2" options={{ max : 25 }} style={{ height: 250, width: 250 }} >
-                <div className="Tilt-inner"> 👽 </div>
+                <div className="Tilt-inner "  >
+                    <img  alt="Logo" src ={brain} /> 
+                    </div>
             </Tilt>
             
         </div>
